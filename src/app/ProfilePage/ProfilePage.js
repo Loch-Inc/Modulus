9201c6a29eff823bb2a0e339bf8d6aa98d8f7af3
adityakumarloch <aditya@loch.one>
2024-10-24 10:55:21 +0530

    Toast changed

--- src/app/ProfilePage/ProfilePage.js
+++ src/app/ProfilePage/ProfilePage.js
@@ -607,7 +607,7 @@ class ProfilePage extends BaseReactComponent {
     navigator.clipboard
       .writeText(shareMessage)
       .then(() => {
-        toast.success("Share the copied referral link with your friends");
+        toast.success("Invitation link copied to clipboard");
       })
       .catch((err) => {
         console.error("Failed to copy share message: ", err);
